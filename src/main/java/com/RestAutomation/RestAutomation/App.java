3b1131c982d27e6eb993c60e5b3c9189823879e7
Yugandhara Sai Kuntala <yugandharasai13@gmail.com>
2020-04-03 07:24:08 +0530

    first commit

---
+++ src/main/java/com/RestAutomation/RestAutomation/App.java
@@ -0,0 +1,13 @@
+package com.RestAutomation.RestAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
